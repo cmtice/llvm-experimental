fba96fe97e429de4a442542a6125436209bd7bdb
tcwang <tcwang@google.com>
2017-06-13 17:04:54 -0700

    Create FRG for functions for all structs

--- lib/Transforms/IPO/StructFieldCacheAnalysis.cpp
+++ lib/Transforms/IPO/StructFieldCacheAnalysis.cpp
@@ -26,6 +26,8 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO.h"
 
+#include <queue>
+#include <unordered_set>
 #include <unordered_map>
 #include <vector>
 
@@ -55,6 +57,77 @@ INITIALIZE_PASS_END(StructFieldCacheAnalysisPass, "struct-field-cache-analysis",
 ModulePass *llvm::createStructFieldCacheAnalysisPass() { return new StructFieldCacheAnalysisPass; }
 
 namespace llvm{
+class FieldReferenceGraph
+{
+ public:
+  struct Edge;
+  struct Node{
+    Node(unsigned I, unsigned N): Id(I), FieldNum(N) {}
+    unsigned Id;
+    unsigned FieldNum;
+    std::unordered_set<Edge*> InEdges;
+    std::unordered_set<Edge*> OutEdges;
+  };
+
+  struct Edge{
+   public:
+    Edge(unsigned I, unsigned C, unsigned D): Id(I), ExecutionCount(C), DataSize(D){}
+    void connectNodes(Node* From, Node* To) { FromNode = From; ToNode = To; }
+    unsigned Id;
+    unsigned ExecutionCount;
+    unsigned DataSize;
+    Node* FromNode;
+    Node* ToNode;
+  };
+
+  struct BasicBlockHelperInfo{
+    BasicBlockHelperInfo(): RemainBytes(0), FirstNode(NULL), LastNode(NULL) {}
+    unsigned RemainBytes;
+    Node* FirstNode;
+    Node* LastNode;
+  };
+
+ public:
+  FieldReferenceGraph(const Function* F) : Func(F), EntryNode(NULL) {}
+  ~FieldReferenceGraph() {
+    for (auto *N : NodeList)
+      delete N;
+    for (auto *E : EdgeList)
+      delete E;
+    for (auto &it : BBInfoMap)
+      delete it.second;
+    NodeList.clear();
+    EdgeList.clear();
+    BBInfoMap.clear();
+  }
+
+  // Creator and getter of helper info for the basic block, useful when connect nodes from different basic blocks
+  BasicBlockHelperInfo* createBasicBlockHelperInfo(const BasicBlock* BB);
+  BasicBlockHelperInfo* getBasicBlockHelperInfo(const BasicBlock* BB);
+
+  // The two functions are used to create a new node in the graph, unconnected with other nodes, and return the pointer to the Node
+  Node* createNewNode(unsigned FieldNum);
+  Node* createNewNode() { return createNewNode(0); }
+
+  // The two functions are used to connect two nodes in FRG with or without given weight
+  void connectNodes(Node* From, Node* To, unsigned C, unsigned D);
+  void connectNodes(Node* From, Node* To) { connectNodes(From, To, 0, 0); }
+
+  // The getter and setter of entry node in the FRG
+  Node* getEntryNode() const { return EntryNode; }
+  void setEntryNode(Node* N) { EntryNode = N; }
+
+  // For debug
+  void debugPrint(raw_ostream& OS) const;
+
+ private:
+  const Function* Func;
+  Node* EntryNode;
+  std::vector<Node*> NodeList;
+  std::vector<Edge*> EdgeList;
+  std::unordered_map<const BasicBlock*, BasicBlockHelperInfo*> BBInfoMap;
+};
+
 class StructFieldAccessInfo;
 class StructFieldAccessManager
 {
@@ -65,6 +138,8 @@ class StructFieldAccessManager
  public:
   StructFieldAccessManager(const Module& M, function_ref<BlockFrequencyInfo *(Function &)> L):
       CurrentModule(M), LookupBFI(L), StatCounts(stats::max_stats) {};
+  ~StructFieldAccessManager();
+  // Functions for IR analysis
   // Check if the struct type is created before; if not, create a new StructFieldAccessInfo object for it
   StructFieldAccessInfo* createOrGetStructFieldAccessInfo(const Type* T);
   // Retrieve the pointer to the previous created StructFieldAccessInfo object for the type
@@ -76,10 +151,18 @@ class StructFieldAccessManager
   }
   // Retrive a pair of information if the instruction is accessing any struct type and field number
   Optional<std::pair<const Type*, unsigned> > getFieldAccessOnInstruction(const Instruction* I) const;
+
+  // Functions for FRG build
+  // Call functions to build FRG for all structs with accesses
+  void buildFieldReferenceGraphForAllStructs();
+
+  // Functions for debugging
   // Print all accesses of all struct types defined in the program
-  void debugPrintAllStructAccesses();
+  void debugPrintAllStructAccesses() const;
+  // Print all FRGs of all struct types
+  void debugPrintAllFRGs() const;
   // Print the IR of the module with annotated information about struct access
-  void debugPrintAnnotatedModule();
+  void debugPrintAnnotatedModule() const;
 
   // For stats
   enum stats{
@@ -116,7 +199,15 @@ class StructFieldAccessInfo
     essential information for cache-aware struct field analysis.
   */
  public:
-  StructFieldAccessInfo(const Type* T, const StructFieldAccessManager* M): StructureType(dyn_cast<StructType>(T)), NumElements(StructureType->getNumElements()), StructManager(M), StatCounts(StructFieldAccessManager::stats::max_stats) {}
+  StructFieldAccessInfo(const Type* T, const StructFieldAccessManager* SM, const Module& M): StructureType(dyn_cast<StructType>(T)), NumElements(StructureType->getNumElements()), StructManager(SM), CurrentModule(M), StatCounts(StructFieldAccessManager::stats::max_stats) {}
+  ~StructFieldAccessInfo() {
+    for (auto* it : FRGArray){
+      delete it;
+    }
+    FRGArray.clear();
+  }
+
+  // Functions for IR analysis
   // Analyze a value pointing to a struct and collect struct access from it. It can be allocas/function args/globals
   void analyzeUsersOfStructValue(const Value* V);
   // Analyze a value pointing to a struct* and collect struct access from it. It can be allocas/function args/globals
@@ -132,8 +223,15 @@ class StructFieldAccessInfo
   Optional<uint64_t> getExecutionCount(const Instruction* I) const {
     return StructManager->getExecutionCount(I->getParent());
   }
+
+  // Functions for building FRG
+  void buildFieldReferenceGraph();
+  void buildFieldReferenceGraph(const Function* FOA);
+
   // Print all instructions that access any struct field
-  void debugPrintAllStructAccesses(raw_ostream& OS);
+  void debugPrintAllStructAccesses(raw_ostream& OS) const;
+  // Print all FRGs for this struct
+  void debugPrintFieldReferenceGraph(raw_ostream& OS) const;
 
   // For stats
   void addStats(unsigned Category) { StatCounts[Category]++; }
@@ -143,8 +241,14 @@ class StructFieldAccessInfo
   const StructType* StructureType;
   unsigned NumElements;
   const StructFieldAccessManager* StructManager;
-  //A map records all instructions accessing which field of the structure
+  const Module& CurrentModule;
+  // A map records all instructions accessing which field of the structure
   std::unordered_map<const Instruction*, unsigned> FieldAccessMap;
+  // A vector stores functions that contains struct accesses for further analysis
+  std::unordered_set<const Function*> FunctionsForAnalysis;
+  // A vector stores all FRGs for all functions
+  std::vector<FieldReferenceGraph*> FRGArray;
+  //A vector stores per struct stats
   std::vector<unsigned> StatCounts;
 
   // Private functions
@@ -200,13 +304,63 @@ class StructFieldCacheAnalysisAnnotatedWriter : public AssemblyAnnotationWriter 
 };
 }
 
+// Functions for FieldReferenceGraph
+FieldReferenceGraph::BasicBlockHelperInfo* FieldReferenceGraph::createBasicBlockHelperInfo(const BasicBlock* BB)
+{
+  assert (BBInfoMap.find(BB) == BBInfoMap.end());
+  auto* ret = BBInfoMap[BB] = new BasicBlockHelperInfo;
+  return ret;
+}
+FieldReferenceGraph::BasicBlockHelperInfo* FieldReferenceGraph::getBasicBlockHelperInfo(const BasicBlock* BB)
+{
+  assert (BBInfoMap.find(BB) != BBInfoMap.end());
+  return BBInfoMap[BB];
+}
+FieldReferenceGraph::Node* FieldReferenceGraph::createNewNode(unsigned FieldNum)
+{
+  auto* Node = new FieldReferenceGraph::Node(NodeList.size(), FieldNum);
+  NodeList.push_back(Node);
+  return Node;
+}
+void FieldReferenceGraph::connectNodes(FieldReferenceGraph::Node* From, FieldReferenceGraph::Node* To, unsigned C, unsigned D)
+{
+  auto* Edge = new FieldReferenceGraph::Edge(EdgeList.size(), C, D);
+  EdgeList.push_back(Edge);
+  Edge->connectNodes(From, To);
+  From->OutEdges.insert(Edge);
+  To->InEdges.insert(Edge);
+}
+
+void FieldReferenceGraph::debugPrint(raw_ostream& OS) const
+{
+  assert(EntryNode);
+  std::queue<Node*> ExamineList; // List of Nodes to print
+  std::unordered_set<Node*> ExaminedSet; // Set of Nodes popped from list to avoid duplicate
+  ExamineList.push(EntryNode);
+  OS << "Field Reference Graph for function: " << Func->getName() << "\n";
+  while (!ExamineList.empty()){
+    auto* Node = ExamineList.front();
+    ExamineList.pop();
+    if (ExaminedSet.find(Node) != ExaminedSet.end())
+      continue;
+    ExaminedSet.insert(Node);
+    OS << "Node " << Node->Id << " accesses " << Node->FieldNum << ": ";
+    OS << "connect with {";
+    for (auto* Edge : Node->OutEdges){
+      OS << " Node " << Edge->ToNode->Id << " (" << Edge->ExecutionCount << "," << Edge->DataSize << ")  ";
+      ExamineList.push(Edge->ToNode);
+    }
+    OS << "}\n";
+  }
+}
+
 // Functions for StructFieldAccessInfo
 void StructFieldAccessInfo::addFieldAccessNum(const Instruction* I, unsigned FieldNum)
 {
   assert(I->getOpcode() == Instruction::Load || I->getOpcode() == Instruction::Store); // Only loads and stores
   assert (FieldAccessMap.find(I) == FieldAccessMap.end());
   FieldAccessMap[I] = FieldNum;
-
+  FunctionsForAnalysis.insert(I->getParent()->getParent()); // If this function already in the set, it'll be ignored by the set::insert
   /*
   // Make sure the field type matches the instruction type
   Type* Ty;
@@ -350,8 +504,98 @@ void StructFieldAccessInfo::analyzeUsersOfStructPointerValue(const Value* V)
   }
 }
 
+void StructFieldAccessInfo::buildFieldReferenceGraph(const Function* F)
+{
+  DEBUG(dbgs() << "Create a new empty FRG\n");
+  auto* FRG = new FieldReferenceGraph(F);
+  FRGArray.push_back(FRG);
+  // Create and connect node inside each basic block
+  for (auto &BB : *F){
+    DEBUG(dbgs() << "Build partial FRG for BB: " << BB << "\n");
+    auto* BBI = FRG->createBasicBlockHelperInfo(&BB);
+    for (auto &I : BB){
+      if (auto FieldNum = getAccessFieldNum(&I)){
+        // Case that I is a struct access
+        DEBUG(dbgs() << "Found an instruction " << I << " is a struct access on field [" << FieldNum.getValue() << "]\n");
+        auto* NewNode = FRG->createNewNode(FieldNum.getValue());
+        if (BBI->LastNode){
+          DEBUG(dbgs() << "Previous nodes found in the BB\n");
+          auto C = 0;
+          if (auto ExCnt = getExecutionCount(&I))
+            C = getExecutionCount(&I).getValue();
+          auto D = BBI->RemainBytes;
+          FRG->connectNodes(BBI->LastNode, NewNode, C, D);
+          BBI->RemainBytes = 0;
+          DEBUG(dbgs() << "Connect new node with previous node in the BB: Field [" << BBI->LastNode->FieldNum << "] to [" << FieldNum.getValue() << "] with count: " << C << " and data " << D << " bytes \n");
+          BBI->LastNode = NewNode;
+        }
+        else{
+          DEBUG(dbgs() << "No previous node found. It is the first node\n");
+          BBI->FirstNode = BBI->LastNode = NewNode;
+        }
+      }
+      else{
+        // Case that I is not struct access but a memory access
+        if (I.getOpcode() == Instruction::Load || I.getOpcode() == Instruction::Store){
+          DEBUG(dbgs() << "Found an instruction " << I << " is not a struct access but a load/store.\n");
+          if (BBI->LastNode == NULL){
+            // Create a dummy node for the first non-struct memory access
+            DEBUG(dbgs() << "Create a dummy node as the first node in the BB.\n");
+            BBI->FirstNode = BBI->LastNode = FRG->createNewNode();
+          }
+          else{
+            Type* type;
+            if (I.getOpcode() == Instruction::Load)
+              type = I.getType();
+            else
+              type = I.getOperand(0)->getType();
+            assert (type->isSized());
+            BBI->RemainBytes += CurrentModule.getDataLayout().getTypeSizeInBits(type) / 8;
+            DEBUG(dbgs() << "Increment remaining data size to " << BBI->RemainBytes << "\n");
+          }
+        }
+      }
+    }
+    if (BBI->LastNode == NULL){
+      assert(BBI->FirstNode == NULL);
+      DEBUG(dbgs() << "Create a dummy node as BB does not have memory accesses\n");
+      BBI->FirstNode = BBI->LastNode = FRG->createNewNode();
+    }
+  }
+  // Connect nodes between different basic blocks
+  for (auto &BB : *F){
+    auto* BBI = FRG->getBasicBlockHelperInfo(&BB);
+    auto* Term = BB.getTerminator();
+    for (const auto *SB : Term->successors()){
+      auto* SBI = FRG->getBasicBlockHelperInfo(SB);
+      unsigned C = 0;
+      auto BBCount = getExecutionCount(&BB), SBCount = getExecutionCount(SB);
+      if (BBCount.hasValue() && SBCount.hasValue())
+        C = std::min(BBCount.getValue(), SBCount.getValue());
+      auto D = BBI->RemainBytes; // Use size of remaining data in BB
+      FRG->connectNodes(BBI->LastNode, SBI->FirstNode, C, D);
+    }
+  }
+  auto* BBI = FRG->getBasicBlockHelperInfo(&F->getEntryBlock());
+  FRG->setEntryNode(BBI->FirstNode);
+}
 
-void StructFieldAccessInfo::debugPrintAllStructAccesses(raw_ostream& OS)
+void StructFieldAccessInfo::buildFieldReferenceGraph()
+{
+  for (auto *F : FunctionsForAnalysis){
+    outs() << "Gonna build FRG for Function " << F->getName() << "\n";
+    buildFieldReferenceGraph(F);
+  }
+}
+
+void StructFieldAccessInfo::debugPrintFieldReferenceGraph(raw_ostream& OS) const
+{
+  for (auto *FRG : FRGArray){
+    FRG->debugPrint(OS);
+  }
+}
+
+void StructFieldAccessInfo::debugPrintAllStructAccesses(raw_ostream& OS) const
 {
   for (auto &it : FieldAccessMap){
     OS << "\tInstruction [" << *it.first << "] accesses field number [" << it.second << "]\n";
@@ -359,13 +603,19 @@ void StructFieldAccessInfo::debugPrintAllStructAccesses(raw_ostream& OS)
 }
 
 // Functions for StructFieldAccessManager
+StructFieldAccessManager::~StructFieldAccessManager() {
+  for (auto &it : StructFieldAccessInfoMap)
+    delete it.second;
+  StructFieldAccessInfoMap.clear();
+  DEBUG(dbgs() << "Finish StructFieldAccessManager destructor\n");
+}
 StructFieldAccessInfo* StructFieldAccessManager::createOrGetStructFieldAccessInfo(const Type* T)
 {
   if (auto* def = getStructFieldAccessInfo(T))
     return def;
   else{
     assert(T->isStructTy() && isa<StructType>(T));
-    def = StructFieldAccessInfoMap[T] = new StructFieldAccessInfo(T, this);
+    def = StructFieldAccessInfoMap[T] = new StructFieldAccessInfo(T, this, CurrentModule);
     return def;
   }
 }
@@ -390,7 +640,16 @@ Optional<std::pair<const Type*, unsigned> > StructFieldAccessManager::getFieldAc
   return ret;
 }
 
-void StructFieldAccessManager::debugPrintAllStructAccesses()
+void StructFieldAccessManager::buildFieldReferenceGraphForAllStructs()
+{
+  for (auto& it : StructFieldAccessInfoMap){
+    if (it.second->getTotalNumFieldAccess() != 0){
+      it.second->buildFieldReferenceGraph();
+    }
+  }
+}
+
+void StructFieldAccessManager::debugPrintAllStructAccesses() const
 {
   dbgs() << "------------ Printing all struct accesses: ---------------- \n";
   for (auto &it : StructFieldAccessInfoMap){
@@ -410,7 +669,27 @@ void StructFieldAccessManager::debugPrintAllStructAccesses()
   dbgs() << "----------------------------------------------------------- \n";
 }
 
-void StructFieldAccessManager::debugPrintAnnotatedModule()
+void StructFieldAccessManager::debugPrintAllFRGs() const
+{
+  dbgs() << "------------ Printing all FRGs: ------------------- \n";
+  for (auto &it : StructFieldAccessInfoMap){
+    dbgs().changeColor(raw_ostream::YELLOW);
+    auto* type = it.first;
+    assert(isa<StructType>(type));
+    if (dyn_cast<StructType>(type)->isLiteral()){
+      dbgs() << "A literal struct has FRGs: \n";
+    }
+    else{
+      dbgs() << "Struct [" << type->getStructName() << "] has FRGs: \n";
+    }
+    dbgs().changeColor(raw_ostream::GREEN);
+    it.second->debugPrintFieldReferenceGraph(dbgs());
+    dbgs().resetColor();
+  }
+  dbgs() << "----------------------------------------------------------- \n";
+}
+
+void StructFieldAccessManager::debugPrintAnnotatedModule() const
 {
   StructFieldCacheAnalysisAnnotatedWriter Writer(this);
   std::error_code EC;
@@ -542,6 +821,13 @@ static void performIRAnalysis(Module &M,
   StructManager->printStats();
 }
 
+static void createFieldReferenceGraph(StructFieldAccessManager* StructManager)
+{
+  //FIXME: add a filter to filter out structs to ignore
+  StructManager->buildFieldReferenceGraphForAllStructs();
+  DEBUG(StructManager->debugPrintAllFRGs());
+}
+
 static bool performStructFieldCacheAnalysis(Module &M,
                                             function_ref<BlockFrequencyInfo *(Function &)> LookupBFI)
 {
@@ -549,6 +835,8 @@ static bool performStructFieldCacheAnalysis(Module &M,
   StructFieldAccessManager StructManager(M, LookupBFI);
   // Step 1 - perform IR analysis to collect info of all structs
   performIRAnalysis(M, &StructManager);
+  // Step 2 - create Field Refernce Graph according to the results of IR analysis
+  createFieldReferenceGraph(&StructManager);
 
   DEBUG(dbgs() << "End of struct field cache analysis\n");
   return false;
